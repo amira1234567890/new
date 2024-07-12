51ca473fe6391e1a1dd2f18ccc5caeb1e111ee29
Amira Natasha Binti Abdullah Harun <v-ambintiabd@microsoft.com>
2024-07-12 09:03:32 +0000

    Add project files.

---
+++ AspireApp4.AppHost/Program.cs
@@ -0,0 +1,9 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.AspireApp4_ApiService>("apiservice");
+
+builder.AddProject<Projects.AspireApp4_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(apiService);
+
+builder.Build().Run();
